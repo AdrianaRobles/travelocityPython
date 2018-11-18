44cdff3182573b2ddc576b2122b7b79a572a0b67
aldovillalbarodriguez <44008834+aldovillalbarodriguez@users.noreply.github.com>
2018-11-17 20:18:53 -0600

    change to first test

--- travelocityPython/pages/__init__.py
+++ travelocityPython/pages/__init__.py
@@ -3,12 +3,29 @@ from selenium import webdriver
 class BasePage():
     def __init__(self,driver):
         self.driver = driver
-            
     
     def getDriver(self):
         return self.driver
+
     def dispose(self):
-        self.driver.close()      
+        self.driver.close() 
+        
+    def monthToNum(self,shortMonth):
+        return{
+            'Jan' : 1,
+            'Feb' : 2,
+            'Mar' : 3,
+            'Apr' : 4,
+            'May' : 5,
+            'Jun' : 6,
+            'Jul' : 7,
+            'Aug' : 8,
+            'Sep' : 9, 
+            'Oct' : 10,
+            'Nov' : 11,
+            'Dec' : 12
+        }[shortMonth]
+    
         
 
         
--- travelocityPython/pages/travelocityPageHome.py
+++ travelocityPython/pages/travelocityPageHome.py
@@ -1,26 +1,62 @@
 from pages import BasePage
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains as AC 
 '''
 Created on 15/11/2018
 
 @author: aldo.villalba
 '''
-from selenium.webdriver.support import expected_conditions
-
 
 class TravelocityHome(BasePage):
     
     def __init__(self,driver):
         super().__init__(driver)
         self.driver.get("https://www.travelocity.com")
-    
+        self.botonVuelos=self.driver.find_element_by_id("tab-flight-tab-hp")
+        self.selectFrom=self.driver.find_element_by_id("flight-origin-hp-flight")
+        self.selectTo = self.driver.find_element_by_id("flight-destination-hp-flight")
+        self.calendarFrom = self.driver.find_element_by_id("flight-departing-hp-flight")
+        self.calendarTo=self.driver.find_element_by_id("flight-returning-hp-flight")
+        
     def findFly(self):
-        botonVuelos=self.driver.find_element_by_id("tab-flight-tab-hp")
-        botonVuelos.click()
-        selectFrom=self.driver.find_element_by_id("flight-origin-hp-flight")
-        selectFrom.click()
-        selectFrom.send_keys("LAS")
-        listFrom=self.driver.find_element_by_id("typeaheadDataPlain")
-        self.driver.wait().until(expected_conditions.visibility_of_all_elements_located(listFrom))
+        self.botonVuelos.click()
+        self.selectFrom.click()
+        self.selectFrom.send_keys("LAS")
+        WebDriverWait(self.driver,10).until(EC.visibility_of_all_elements_located((By.ID,"typeaheadDataPlain")))  
+        lasElementFrom = self.driver.find_element_by_xpath("//ul//li/a[@data-value='Las Vegas, NV (LAS-All Airports)']") 
+        lasElementFrom.click()
+        self.selectTo.click()
+        self.selectTo.send_keys("LAX");
+        WebDriverWait(self.driver,10).until(EC.visibility_of_all_elements_located((By.ID,"typeaheadDataPlain")))  
+        laxElementTo = self.driver.find_element_by_xpath("//ul//li/a[@data-value='Los Angeles, CA (LAX-Los Angeles Intl.)']")
+        laxElementTo.click()
+        
+        self.calendarFrom.click()
+        self.selectDayFromAndTo("Feb 2019","14-Feb-2019")
+        self.calendarTo.click()
+        self.selectDayFromAndTo("Feb 2019","18-Feb-2019")
+        
+    def selectDayFromAndTo(self,month,choicedDay):
+        
+        WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"div.datepicker-cal button.next")))
+        
+        while True:
+            nameMonth=self.driver.find_element_by_css_selector("caption.datepicker-cal-month-header")
+            print(nameMonth.text)
+            if nameMonth.text!=month:
+                buttonNextMonth=self.driver.find_element_by_css_selector("div.datepicker-cal button.next")
+                WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"div.datepicker-cal button.next")))
+                buttonNextMonth.click()
+            elif nameMonth.text==month:
+                break
+        splitDate=choicedDay.split("-")  
+        monthReturn=self.monthToNum(splitDate[1])
+        
+        stringXpath="//button[@data-year='"+splitDate[2]+"'][@data-month='"+str(monthReturn)+"'][@data-day="+splitDate[0]+"]"
+        buttonSinceDay = self.driver.find_element_by_xpath(stringXpath)
+        buttonSinceDay.click()
+            
         
         
